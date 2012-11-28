756c1986a49208bc10355e0595e9a29c17f59487
Rainux Luo <rainux@gmail.com>
2012-11-28 11:32:02 +0800

    Try to use JCAutocompletingSearch

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,36 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    searchController = JCAutocompletingSearchViewController.autocompletingSearchViewController
+    searchController.delegate = self
+
+    navigationController = UINavigationController.alloc.initWithRootViewController(searchController)
+    navigationController.navigationBarHidden = true
+    window.rootViewController = navigationController
+
+    window.makeKeyAndVisible
+
     true
+  end
+
+  def window
+    @window ||= UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+  end
+
+  def searchController(searchController, performSearchForQuery: query, withResultsHandler: resultsHandler)
+    p 'searchController'
+
+    resultsHandler([
+      {label: 'Result 1'},
+      {label: 'Result 2'},
+      {label: 'Result 3'}
+    ])
+  end
+
+  def searchControllerCancelled(searchController)
+    p 'Canceled'
+  end
+
+  def searchController(searchController, tableView: tableView, selectedResult: result)
+    p result
   end
 end
